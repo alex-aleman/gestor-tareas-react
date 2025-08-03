27d5ef5fb203fbe26f7c8c056601aebf3b55ae1d
alex-aleman <g.bodden.a96@outlook.com>
2025-08-03 05:43:37 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gestor-tareas-react/sw.js', { scope: '/gestor-tareas-react/' })})}
